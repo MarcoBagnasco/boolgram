e87cb44a0d32d0f120101d2dfeca6c080bfeeb4f
MarcoBagnasco <bagna.hbk@gmail.com>
2021-08-20 17:59:41 +0200

    style nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="main-nav">
         <nav class="container flex jc-between ai-center">
-            <div>Logo</div>
-            <div>Cerca</div>
-            <div class="flex">
-                <div>heart</div>
-                <div>home</div>
-                <div>Me</div>
+            <!-- Logo -->
+            <img class="logo" src="../assets/logo.png" alt="logo">
+
+            <!-- Search -->
+            <input type="text" placeholder="Cerca">
+
+            <!-- Profile's Icons -->
+            <div class="icons flex ai-center">
+                <i class="far fa-heart"></i>
+                <i class="fas fa-home"></i>
+                <img class="profile" src="../assets/profile.jpg" alt="profile">
             </div>
         </nav>
     </div>
@@ -27,5 +32,30 @@ export default {
         padding: 15px;
         background-color: #fff;
         border-bottom: 1px solid #bbb;
+
+        .logo{
+            max-height: 40px;
+        }
+
+        input[type="text"]{
+            appearance: none;
+            width: 200px;
+            padding: 8px;
+            text-align: center;
+            border: 1px solid #bbb;
+        }
+
+        .icons{
+            font-size: 1.3rem;
+
+            i{
+                margin-right: 20px;
+            }
+            
+            .profile{
+                height: 40px;
+                border-radius: 50%;
+            }
+        }
     }
 </style>
